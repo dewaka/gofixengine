9dce78ffed046facdaa5bf53a0754f2b40cebb69
dewaka <dcdewaka@gmail.com>
2013-08-05 23:51:34 +0100

    Basic Field definitions - more tests

--- fields/fields_test.go
+++ fields/fields_test.go
@@ -21,7 +21,30 @@ func TestStringFields(t *testing.T) {
 		t.Errorf("Field is of type String, but int value retrievable")
 	} else {
 		if i != 0 {
-			t.Errorf("Did not get the default value 0 on failure")
+			t.Errorf("Did not get the default value 0 on failure, instead got %d", i)
+		}
+	}
+}
+
+func TestIntFields(t *testing.T) {
+	num := 349
+	s := IntField(900, num) // tag is 900, value is num
+	n, err := s.GetInt()
+	if err != nil {
+		t.Errorf("Failed to retrieve int value")
+	} else {
+		if n != num {
+			t.Errorf("Extracted value %d but instead got %d", num, n)
+		}
+	}
+
+	// Negative test - this should fail
+	str, err2 := s.GetString()
+	if err2 == nil {
+		t.Errorf("Field is of type Int, but string value retrievable")
+	} else {
+		if str != "" {
+			t.Errorf("Did not get the default value \"\" on failure, instead got %s", str)
 		}
 	}
 }
