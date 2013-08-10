04369fa1f473992e4362b00af5f4799047fabade
dewaka <dcdewaka@gmail.com>
2013-08-10 01:15:13 +0100

    Added basic requirements for FieldMap type

--- message/message.go
+++ message/message.go
@@ -4,6 +4,9 @@ import (
 	"github.com/dewaka/gofixengine/fields"
 )
 
+// FieldMap is the basic Field contianer used by Messages and Groups
+// Should preserve field ordering
+// Should have fast lookup based on tag value for fields
 type FieldMap struct {
 	fmap map[int]*fields.Field
 }
